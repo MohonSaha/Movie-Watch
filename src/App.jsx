3eee3d0e56fc8fd785755990e42e013f46457e7b
MohonSaha <practicewithmohon@gmail.com>
2023-03-30 12:21:36 +0600

    add local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
@@ -8,15 +8,18 @@ import Contract from './components/Contract/Contract';
 
 const App = () => {
 
+  const [watchTime, setWatchTime] = useState("");
 
   const handleWatchTime = (time) =>{
     const previousWatchTime = JSON.parse(localStorage.getItem("watchTime"));
     if(previousWatchTime){
       const sum = previousWatchTime + time;
       localStorage.setItem("watchTime", sum);
+      setWatchTime(sum);
     }
     else{
       localStorage.setItem("watchTime", time);
+      setWatchTime(time);
     }
   }
 
@@ -35,7 +38,7 @@ const App = () => {
           <Home handleWatchTime={handleWatchTime}></Home>
         </div>
         <div className='sideCart col-md-4'>
-          <SideCart></SideCart>
+          <SideCart watchTime={watchTime}></SideCart>
         </div>
       </div>
 
--- src/components/SideCart/SideCart.jsx
+++ src/components/SideCart/SideCart.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const SideCart = () => {
+const SideCart = ({watchTime}) => {
+
+    const [time, setTime] = useState(watchTime);
+    useEffect(() => {
+        const getWatchTimeFromStorage = localStorage.getItem("watchTime");
+        setTime(getWatchTimeFromStorage);
+    }, [watchTime])
+
+
     return (
         <div className='card'>
-            <h1>This is side cart Lorem ipsum dolor sit amet.</h1>
+            <h1>My Cart</h1>
+
+            <div className='mt-5 text-center mb-4'>
+                <p>Total Watch Time</p>
+                <input type="text" value={time} disabled />
+            </div>
+            <h5>Add Break Time</h5>
+            <button className='w-25 bt-circle m-1 btn btn-info'>15</button>
+            <button className='w-25 bt-circle m-1 btn btn-warning'>20</button>
+            <button className='w-25 bt-circle m-1 btn btn-danger'>25</button>
+            <input type="text" value={0} disabled />
+            <button className='w- bt-circle m-1 btn btn-info'>Complete</button>
         </div>
     );
 };
